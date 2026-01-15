26119500fe7cdad31ac8218237a04fc1e480ff9c
KhaiLe190904 <lequangkhai190904@gmail.com>
2026-01-15 15:24:08 +0700

    feat: add search with Apache Lucene & Hibernate Search and fix bug in comment modal

--- be/build.gradle.kts
+++ be/build.gradle.kts
@@ -25,17 +25,33 @@ repositories {
 }
 
 dependencies {
+	// Resend mail
 	implementation("com.resend:resend-java:+")
+
+	// Database
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+
+	// jwt
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+	// websocket
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+	// Search
+	implementation("org.hibernate.search:hibernate-search-mapper-orm:7.2.2.Final")
+	implementation("org.hibernate.search:hibernate-search-backend-lucene:7.2.2.Final")
+	implementation("org.jboss.logging:jboss-logging:3.6.1.Final")
+
+	// DevTools
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
+
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
